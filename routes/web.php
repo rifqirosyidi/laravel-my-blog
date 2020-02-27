85a11fc2b51e543bf38ba1358f86d563d0e57578
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2020-02-27 18:24:31 +0700

    basic controller

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/about', function() {
-	return view('about');
-});
-
-Route::get('/contact', function() {
-	return view('contact');
-});
+Route::get('/', 'PagesController@getIndex');
+Route::get('/about', 'PagesController@getAbout');
+Route::get('/contact', 'PagesController@getContact');
